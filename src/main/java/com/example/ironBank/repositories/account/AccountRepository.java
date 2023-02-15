1f3f858bcb9484c4c08007568d2032ae7f4bf7b2
Lidia Ros <lidiarosmontes@gmail.com>
2023-02-15 10:33:39 +0100

    repos

---
+++ src/main/java/com/example/ironBank/repositories/account/AccountRepository.java
@@ -0,0 +1,4 @@
+package com.example.ironBank.repositories.account;
+
+public class AccountRepository {
+}
---
+++ src/main/java/com/example/ironBank/repositories/user/AccountHoldersRepository.java
@@ -0,0 +1,4 @@
+package com.example.ironBank.repositories.user;
+
+public class AccountHoldersRepository {
+}
